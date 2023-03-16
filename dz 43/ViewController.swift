05b77c2b910b33f7722d0307fb4e4b51f8040bd0
Memka <mkasenova87@gmail.com>
2023-03-16 19:25:52 +0600

    Initial Commit

---
+++ dz 43/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dz 43
+//
+//  Created by merim kasenova on 16/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
